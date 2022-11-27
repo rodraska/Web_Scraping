7db11a19d5739a5ba86efcbdde0948495f117d56
Rodrigo Reis De Carvalho <rreis-de@c2r4s3.42lisboa.com>
2022-11-27 17:40:43 +0000

    scraping man

--- WC2022/a.py
+++ WC2022/a.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
-print('Escolhe o jogador')
+print('Pick a Player')
 homem = input('>')
 
-html_text = requests.get('https://www.zerozero.pt/equipa.php?id=811&edicao_id=132894').text
+html_text = requests.get('https://www.playmakerstats.com/equipa.php?id=961&edicao_id=132894').text
 soup = BeautifulSoup(html_text, 'lxml')
 squad = soup.find('div', id = 'team_squad')
 body = squad.find('tbody')
 players = body.find_all('tr')
+names_list = []
+positions_list = []
+ages_list = []
+teams_list = []
 for jogador in players:
     player_td = jogador.find_all('td')
     player_list = player_td[1]
@@ -18,10 +23,27 @@ for jogador in players:
         player_age = player_td[3].text
         player_sheet = player_td[4]
         player_team = player_sheet.find('div', class_ = 'text').text
+        names_list.append(player_name)
+        positions_list.append(player_position)
+        ages_list.append(player_age)
+        teams_list.append(player_team)
 
-        print(f'''
+        """ print(f'''
         Nome: {player_name}
         Posição: {player_position}
         Idade: {player_age}
         Equipa: {player_team}
-        ''')
+        ''') """
+
+data = {
+    'Name': names_list,
+    'Position': positions_list,
+    'Age': ages_list,
+    'Team': teams_list
+}
+
+df = pd.DataFrame(data)
+
+df.to_csv('Ghana.csv', index=False)
+
+print(df)
--- tests/boat.py
+++ tests/boat.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+import matplotlib.pyplot as plt
+
 df = pd.read_csv("titanic.csv")
 
 cols = df.columns.tolist()
@@ -38,4 +40,62 @@ df2 = df2[df2["Age"].notna()]
 
 df2.Age = df2.Age.astype(int)
 
-print(df2)
+names = df.Name.tolist()
+
+revs = []
+for name in names:
+    if "Rev." in name:
+        revs.append(name)
+
+
+reverends = df.loc[df["Name"].str.contains("Rev.")]
+
+not_reverends = df.loc[~df["Name"].str.contains("Rev.")]
+
+rev_or_mr = df.loc[(df["Name"].str.contains("Rev\.")) | (df["Name"].str.contains("Mr\."))]
+
+rev_or_misters = df.loc[df["Name"].str.contains("Rev\.|Mr\.", regex=True)]
+
+new_df = df.filter(["Age", "Name"])
+
+#print(df.filter(["Name"]).Name.str.contains("Miss.").value_counts())
+
+#print(df[df.filter(["Name"]).Name.str.contains("Miss.") == True])
+
+#print(df[df.Name.str.contains("Miss.") == True].filter(["Name", "Age"]))
+
+#print(df.query("Pclass == 3"))
+
+#print(df.query("Pclass == 3 & Survived == 1 & (Age > 40 | Age < 10)"))
+
+#print(df.groupby("Sex").get_group("male"))
+
+#print(df.groupby("Age").get_group(22))
+
+#print(df.groupby("Sex").count())
+
+#print(df.groupby("Sex").sum())
+
+#print(df.groupby("Sex").Fare.sum())
+
+#pd.set_option('display.max_rows', 1000)
+
+#print(df.groupby(["Sex", "Pclass", "Age"]).Fare.sum())
+
+#df["Sex"].value_counts().plot.barh(title="Passengers on Titanic", color=["red", "green"])
+
+#plt.show()
+
+#df["Sex"].value_counts().plot.pie(figsize=(6, 6), labels=["Male", "Female"], autopct="%.2f")
+
+#plt.show()
+
+df.loc[(df.Pclass == 1), "Pclass"] = "First"
+df.loc[(df.Pclass == 2), "Pclass"] = "Second"
+df.loc[(df.Pclass == 3), "Pclass"] = "Third"
+
+df["Pclass"] = df.Pclass.astype("category")
+
+df.plot.scatter(x="Age", y="Fare", c="Pclass", cmap="viridis", figsize=(10, 5))
+
+plt.show()
